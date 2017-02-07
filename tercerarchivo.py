7be4d8b7432f969a33e3d0982fd8f5698d34578c
alejandro-gg <alejandro-gg@users.noreply.github.com>
2017-02-07 17:17:51 -0600

    made changes to the PY file just for kicks...

--- tercerarchivo.py
+++ tercerarchivo.py
@@ -2,3 +2,13 @@
 
 este es el archivo de prueba... con extension manualmente anadida por mi PY
 
+# oh wow so actually it has the same text-font coloring function than Atom , activated when i added the PY extension ... cool  
+# sooo , ... anyway , this line is a commit made on the PY file... just for kicks... so that i can play with the "pull request" feature again
+
+
+
+mivar1 = 5
+mivar2 = 80
+print mivar2 / mivar1
+
+#agl fin
